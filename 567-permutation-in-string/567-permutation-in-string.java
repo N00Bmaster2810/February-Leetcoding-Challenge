aa4d75c067a94fcb1c4a401bce9353631ea6cfcd
Satya Prakash Satyam <61646176+N00Bmaster2810@users.noreply.github.com>
2022-02-16 10:26:04 +0530

    Time: 224 ms (17.40%), Space: 49.8 MB (12.63%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] hash1 = new int[26];
+        for(int i=0;i<s1.length();i++){
+            hash1[s1.charAt(i)-'a']++;
+        }
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(hash1, s2map))
+                return true;
+        }
+        return false;
+    }
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+}
